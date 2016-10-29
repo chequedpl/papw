e38f5745b949445e2c8c6cf599e0c0ea288d41c1
chequedpl <chequedpl@hotmail.com>
2016-10-29 13:47:30 -0500

    Migrations y Seeders

--- database/migrations/2016_10_29_183303_create_User_table.php
+++ database/migrations/2016_10_29_183303_create_User_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('userId')->index();
             $table->string('name');
-            $table->string('description');
-            $table->float('price');
-            $table->string('photo1');
-            $table->timestamps();
+            $table->string('email');
+            $table->string('password');
+            $table->string('date');
+            $table->string('gender');
+            $table->binary('avatar');
+            
         });
     }
 
@@ -31,6 +32,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('users');
     }
 }
