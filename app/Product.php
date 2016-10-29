721d34520034c44e163a9212a8d61767859d6782
chequedpl <chequedpl@hotmail.com>
2016-10-29 08:07:33 -0500

    Holy Moly

---
+++ app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+
+	protected $fillable = [
+	'name', 'description', 'price', 'photo1'
+	];
+
+    public function user(){
+    	return $this->belongsTo(User::class);
+    }
+}
